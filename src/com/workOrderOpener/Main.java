c613ab61acb66ea2da3eb9abe1fcc6e3bd1f0e4b
JD-MBP <jashansingh29@gmail.com>
2024-01-12 21:32:30 -0500

    ;

---
+++ src/com/workOrderOpener/Main.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package com.workOrderOpener;
+
+/**
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println(user());
+
+    }
+
+    public static String user() {
+	return System.getProperty("custom.prop");
+    }
+
+    public static void openFile() {
+
+    }
+
+}
